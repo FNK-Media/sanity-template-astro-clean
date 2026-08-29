5fac2551cec7de9df1e1ca7c2b4eaf3a533e025a
FNK-Media <communication-fnk@outlook.com>
2026-08-29 21:42:21 +0300

    Update astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,10 +2,16 @@
 // https://docs.astro.build/en/guides/configuring-astro/#environment-variables
 import { loadEnv } from "vite";
 const {
+  PUBLIC_SANITY_PROJECT_ID,
+  PUBLIC_SANITY_DATASET,
   PUBLIC_SANITY_STUDIO_PROJECT_ID,
   PUBLIC_SANITY_STUDIO_DATASET,
   PUBLIC_SANITY_STUDIO_URL,
 } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+import { defineConfig } from "astro/config";
+
+const projectId = PUBLIC_SANITY_PROJECT_ID || PUBLIC_SANITY_STUDIO_PROJECT_ID;
+const dataset = PUBLIC_SANITY_DATASET || PUBLIC_SANITY_STUDIO_DATASET;
 import { defineConfig } from "astro/config";
 
 const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID;
